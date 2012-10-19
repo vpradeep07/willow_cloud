1c963fce2ebed247cea771357db0580873138074
Vijay Pradeep <vijay@hidof.com>
2012-10-19 15:29:34 -0700

    Stub for jenkins cache

---
+++ jenkins_cache.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+'''
+Class designed to very closely model the standard jenkins interface,
+but uses the file system to cache many of the server calls
+'''
+class JenkinsCache:
+    def __init__(self):
+        pass
